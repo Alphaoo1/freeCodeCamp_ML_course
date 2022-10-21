92b1d3988d564252d84c4645fa4a40fb0d680a2d
padhav <prasad.adhav@uni.lu>
2022-10-21 17:30:37 +0200

    first commit

---
+++ freeCodeCamp_ML_course/1.py
@@ -0,0 +1,24 @@
+#---------------------------------------------------------------------------------
+# Imports
+#---------------------------------------------------------------------------------
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+import tensorflow as tf
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------
+
+
+print("test")
